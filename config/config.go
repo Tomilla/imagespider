4744840a2f7c072c93a280d25b3f8536ff911b87
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-04-21 02:24:34 +0800

    remove config print

--- config/config.go
+++ config/config.go
@@ -121,14 +121,14 @@ func removeComment(reader io.Reader) (newReader io.Reader) {
 		panic(err)
 	}
 	s := string(bs)
-	fmt.Println("before ", s)
+	//fmt.Println("before ", s)
 	re1 := regexp.MustCompile(`(?im)^\s+\/\/.*$`) // 整行注释
 
 	s = re1.ReplaceAllString(s, "")
-	fmt.Println("after1 ", s)
+	//fmt.Println("after1 ", s)
 	re2 := regexp.MustCompile(`\/\/[^"\[\]]+\n`) // 行末
 	s = re2.ReplaceAllString(s, "")
-	fmt.Println("after2 ", s)
+	//fmt.Println("after2 ", s)
 	newReader = strings.NewReader(s)
 	return
 
