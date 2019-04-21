77bac0250037f864035848d9ea40ef65a1c29e36
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-04-21 23:38:30 +0800

    remove image download for the moment

--- all/all.go
+++ all/all.go
@@ -3,9 +3,8 @@ package all
 import (
 	// for all config
 	_ "github.com/wuxiangzhou2010/imagespider/config"
-
 	// for image config
-	_ "github.com/wuxiangzhou2010/imagespider/downloader/image"
+	//_ "github.com/wuxiangzhou2010/imagespider/downloader/image"
 	// for debug
 	// _ "github.com/wuxiangzhou2010/imagespider/debug"
 )
