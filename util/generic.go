eba674b908592c55342236b5f2c8d4e5433ef563
Carey <gero.rim@gmail.com>
2019-08-21 10:48:39 +0800

    [init] introduce google/go-cmp for study, it will be integrated when I familiar with its API [new] new types(int, uint, float, complex) support in EqualSliceGeneric function
    Signed-off-by: Carey <gero.rim@gmail.com>

--- util/generic.go
+++ util/generic.go
@@ -20,8 +20,9 @@ func EqualSliceGeneric(lhs interface{}, rhs interface{}) (isEqual bool) {
     go func() {
         defer func() {
             if r := recover(); r != nil {
-                fmt.Println("Recovered in f", r)
+                fmt.Println("Recovered in go-routine: ", r)
                 isEqual = false
+                ch <- isEqual
             }
             close(ch)
         }()
@@ -38,6 +39,59 @@ func EqualSliceGeneric(lhs interface{}, rhs interface{}) (isEqual bool) {
                     ch <- false
                     return
                 }
+            case reflect.Int:
+                fallthrough
+            case reflect.Int8:
+                fallthrough
+            case reflect.Int16:
+                fallthrough
+            case reflect.Int32:
+                fallthrough
+            case reflect.Int64:
+                // Int returns v's underlying value, as a largest possible int variant -- int64.
+                if el.Int() != er.Int() {
+                    ch <- false
+                    return
+                }
+                break
+            case reflect.Uint:
+                fallthrough
+            case reflect.Uint8:
+                fallthrough
+            case reflect.Uint16:
+                fallthrough
+            case reflect.Uint32:
+                fallthrough
+            case reflect.Uint64:
+                // Uint returns v's underlying value, as a largest possible uint variant --  uint64.
+                if el.Uint() != er.Uint() {
+                    ch <- false
+                    return
+                }
+                break
+            case reflect.Uintptr:
+                if el.Pointer() != er.Pointer() {
+                    ch <- false
+                    return
+                }
+                break
+            case reflect.Float32:
+                fallthrough
+            case reflect.Float64:
+                // Float returns v's underlying value, as a largest possible float variant  float64e reflejct.Complex64:
+                if el.Float() != er.Float() {
+                    ch <- false
+                    return
+                }
+                break
+            case reflect.Complex64:
+                fallthrough
+            case reflect.Complex128:
+                if el.Complex() != er.Complex() {
+                    ch <- false
+                    return
+                }
+                break
             default:
                 ch <- false
                 return
--- util/generic_test.go
+++ util/generic_test.go
@@ -17,9 +17,45 @@ func TestEqualSliceGeneric(t *testing.T) {
             // test: string slice
             {[]string{"hello", "world"}, []string{"hello", "world"}, true,},
             {[]string{"hello", "world"}, []string{"hello", "tomi"}, false,},
+            {[]string{"hello", "world"}, []string{"hello",}, false,},
+            {[]bool{true, true}, []string{"hello", "world"}, false,},
+            {[]bool{true, true}, []interface{}{"hello", true}, false,},
             // test: boolean slice
             {[]bool{true, true}, []bool{true, true}, true,},
             {[]bool{false, false}, []bool{false, true}, false,},
+            // test: int slice
+            {[]int{1, 3, 5, 7, 9}, []int{1, 3, 5, 7, 9}, true,},
+            {[]int{1, 3, 5, 7, 9}, []int{1, 3, 5, 7, 10}, false,},
+            // test negative int slice
+            {[]int{-1, -3, -5, -7, -9}, []int{-1, -3, -5, -7, -9}, true,},
+            // test: int slice v.s. int16 slice
+            {[]int{1, 3, 5, 7, 9}, []int16{1, 3, 5, 7, 9}, false,},
+            {[]int16{1, 3, 5, 7, 9}, []int16{1, 3, 5, 7, 9}, true,},
+            // test: uint slice
+            {[]uint{1, 3, 5, 7, 9}, []uint{1, 3, 5, 7, 9}, true,},
+            {[]uint{1, 3, 5, 7, 9}, []uint{1, 3, 5, 7, 10}, false,},
+            // test: uint64 slice
+            {[]uint64{1, 3, 5, 7, 9}, []uint64{1, 3, 5, 7, 9}, true,},
+            {[]uint64{1, 3, 5, 7, 9}, []uint64{1, 3, 5, 7, 10}, false,},
+            // test: float32 slice
+            {[]float32{1.2, 3.4, 5.6, 7.8, 9.10}, []float32{1.2, 3.4, 5.6, 7.8, 9.10}, true,},
+            {[]float32{1.2, 3.4, 5.6, 7.8, 9.10}, []float32{1.2, 3.5, 5.7, 7.8, 9.10}, false,},
+            // test: float64 slice
+            {[]float64{1.2, 3.4, 5.6, 7.8, 9.10}, []float64{1.2, 3.4, 5.6, 7.8, 9.10}, true,},
+            {[]float64{1.2, 3.3, 5.6, 7.8, 9.10}, []float64{1.2, 3.3, 5.6, 7.8, 9.10}, true,},
+            // test: complex64 slice
+            {[]complex64{1.2 + 3i, 2.3 + 4i, 3.4 + 5i, 4.5 + 6i, 5.6 + 7i}, []complex64{1.2 + 3i, 2.3 + 4i, 3.4 + 5i, 4.5 + 6i, 5.6 + 7i},
+                true,},
+            {[]complex64{1.2 + 3i, 2.3 + 4i, 3.4 + 5i, 4.5 + 6i, 5.6 + 7i}, []complex64{1.3 + 3i, 2.3 + 4i, 3.4 + 5i,
+                4.5 + 6i, 5.6 + 7i},
+                false,},
+            // test complex128 slice
+            {[]complex128{1.2 + 3i, 2.3 + 4i, 3.4 + 5i, 4.5 + 6i, 5.6 + 7i}, []complex128{1.2 + 3i, 2.3 + 4i, 3.4 + 5i,
+                4.5 + 6i, 5.6 + 7i},
+                true,},
+            {[]complex128{1.2 + 3i, 2.3 + 4i, 3.4 + 5i, 4.5 + 6i, 5.6 + 7i}, []complex128{1.2 + 3i, 2.3 + 4i, 3.4 + 5i,
+                4.5 + 6i, 5.6 + 8i},
+                false,},
         }
     )
     for _, info := range sourceAndExcepted {
